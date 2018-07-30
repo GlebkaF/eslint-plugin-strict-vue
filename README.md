f539fcac4fc9159cd28bae9d84280793b2ed495a
Глебка Фокин <g.fokin@n1.ru>
2018-07-30 23:04:05 +0700

    Create plugin repo

---
+++ README.md
@@ -0,0 +1,51 @@
+# eslint-plugin-n1-vuex-plugin
+
+Набор eslint правил для vuex сторов N1.RU
+
+## Installation
+
+You'll first need to install [ESLint](http://eslint.org):
+
+```
+$ npm i eslint --save-dev
+```
+
+Next, install `eslint-plugin-n1-vuex-plugin`:
+
+```
+$ npm install eslint-plugin-n1-vuex-plugin --save-dev
+```
+
+**Note:** If you installed ESLint globally (using the `-g` flag) then you must also install `eslint-plugin-n1-vuex-plugin` globally.
+
+## Usage
+
+Add `n1-vuex-plugin` to the plugins section of your `.eslintrc` configuration file. You can omit the `eslint-plugin-` prefix:
+
+```json
+{
+    "plugins": [
+        "n1-vuex-plugin"
+    ]
+}
+```
+
+
+Then configure the rules you want to use under the rules section.
+
+```json
+{
+    "rules": {
+        "n1-vuex-plugin/rule-name": 2
+    }
+}
+```
+
+## Supported Rules
+
+* Fill in provided rules here
+
+
+
+
+
---
+++ tests/lib/rules/enforce-vuex-action-doc.js
@@ -0,0 +1,80 @@
+/**
+ * @fileoverview Every action in vuex store should preceded by jsdoc
+ * @author glebkaf
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require("../../../lib/rules/enforce-vuex-action-doc"),
+
+RuleTester = require("eslint").RuleTester;
+
+RuleTester.setDefaultConfig({
+    parserOptions: {
+      ecmaVersion: 6,
+      sourceType: "module"
+    }
+  });
+
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var ruleTester = new RuleTester();
+ruleTester.run("enforce-vuex-action-doc", rule, {
+
+    valid: [     
+            `const store = {
+                actions: {}
+            };`,
+            `const store = {
+                actions: {
+                    /**
+                     * another jsdoc
+                     */
+                    initState() {}
+                }
+            };`,        
+            `function createStore() {
+                return {
+                    actions: {
+                        /**
+                         * Regular jsdoc
+                         */
+                        initState() {}
+                    }
+                };
+            }`        
+    ],
+
+    invalid: [
+        {
+            code: `const store = {
+                actions: {
+                    initState() {}
+                }
+            };`,
+            errors: [{
+                message: "Fill me in.",
+                type: "Me too"
+            }]
+        },
+        {
+            code: `function createStore() {
+                return {
+                    actions: {
+                        initState() {}
+                    }
+                };
+            }`,
+            errors: [{
+                message: "Every method in vuex store should be preceed by jsdoc",
+                type: "Me too"
+            }]
+        },
+    ]
+});
