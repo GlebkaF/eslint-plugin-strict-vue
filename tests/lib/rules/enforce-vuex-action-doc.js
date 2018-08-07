99e1e18a891386bd75369a2af708f5cfbebdecb3
Глебка Фокин <g.fokin@n1.ru>
2018-08-07 19:53:54 +0700

    Collapse some invalid and valid test cases into base cases

--- tests/lib/rules/enforce-vuex-action-doc.js
+++ tests/lib/rules/enforce-vuex-action-doc.js
@@ -16,66 +16,82 @@ const ERROR_MESSAGE = "Actions in vuex store should has jsdoc"
 const PROPERTY_NAME = "actions"
 const VUEX_STORE_CORE_PROPERTY = "state: {}"
 
-/**
- * Specify `only: true` prop. for stanalone testcase run.
- * Specify `skip: true` prop. to skip testcase
- */
-const validCases = [
+// Base test cases are cases that could be both valid and invalid
+// depending on the comment before childProp
+const createBaseCases = ({ comment }) => [
   {
-    code: `const store = {
-      ${VUEX_STORE_CORE_PROPERTY},
-      ${PROPERTY_NAME}: {}
-    };`,
-  },
-  {
-    code: `const store = {
-              ${VUEX_STORE_CORE_PROPERTY},
-              ${PROPERTY_NAME}: {
-                  /**
-                   * another jsdoc
-                   */
-                  initState() {}
-              }
-          };`,
-  },
-  {
+    title: "store object returned by function",
     code: `function createStore() {
-              return {
-                  ${VUEX_STORE_CORE_PROPERTY},
-                  ${PROPERTY_NAME}: {
-                      /**
-                       * Regular jsdoc
-                       */
-                      initState() {}
-                  }
-              };
-          }`,
+            return {
+                ${VUEX_STORE_CORE_PROPERTY},
+                ${PROPERTY_NAME}: {
+                    ${comment}
+                    initState() {}
+                }
+            };
+        }`,
   },
   {
-    title: "Valid key with arrow function",
-    code: `const store = {                
-              ${VUEX_STORE_CORE_PROPERTY},
-              ${PROPERTY_NAME}: {
-                  // some docs
-                  initState: () => {}
-              }
-          };`,
-  },
-  {
-    title: "Valid key with regular function",
+    title: "child prop is es6 function shorthand",
     code: `const store = {
-              ${VUEX_STORE_CORE_PROPERTY},
-              ${PROPERTY_NAME}: {
-                  /* Some docs */
-                  initState: function initState ()  {}
-              }
-          };`,
+            ${VUEX_STORE_CORE_PROPERTY},
+            ${PROPERTY_NAME}: {
+                ${comment}
+                initState() {}
+            }
+        };`,
+  },
+  {
+    title: "child prop is an arrow function",
+    code: `const store = {                
+            ${VUEX_STORE_CORE_PROPERTY},
+            ${PROPERTY_NAME}: {
+                ${comment}
+                initState: () => {}
+            }
+        };`,
+  },
+  {
+    title: "child prop is a regular function",
+    code: `const store = {
+            ${VUEX_STORE_CORE_PROPERTY},
+            ${PROPERTY_NAME}: {
+                ${comment}
+                initState: function initState ()  {}
+            }
+        };`,
   },
   {
     title: "es6 property shorthand syntax",
     code: `
+        const ${PROPERTY_NAME} = {
+            ${comment}
+            initState: function initState ()  {}
+        }
+        
+        const store = {
+            ${VUEX_STORE_CORE_PROPERTY},
+            ${PROPERTY_NAME}
+        };`,
+  },
+  {
+    title: "es6 property shorthand syntax + property export",
+    code: `
+        export const ${PROPERTY_NAME} = {
+            ${comment}
+            initState: function initState ()  {}
+        }
+        
+        const store = {
+            ${VUEX_STORE_CORE_PROPERTY},
+            ${PROPERTY_NAME}
+        };`,
+  },
+  {
+    code: `
+          const justVariableForSettingUpSomeScope = '';
           const ${PROPERTY_NAME} = {
-              /* Some docs */
+                  ${comment}
                   initState: function initState ()  {}
           }
           
@@ -86,49 +102,35 @@ const validCases = [
   },
 ]
 
+const baseValidCases = createBaseCases({ comment: "/** Simple valid jsdoc */" })
+const baseInvalidCases = createBaseCases({ comment: "" }).map(testCase => ({
+  ...testCase,
+  errors: [
+    {
+      message: ERROR_MESSAGE,
+    },
+  ],
+}))
+
+/**
+ * Specify `only: true` prop. for stanalone testcase run.
+ * Specify `skip: true` prop. to skip testcase
+ */
+const validCases = [
+  ...baseValidCases,
+  {
+    title: "empty parent",
+    code: `const store = {
+      ${VUEX_STORE_CORE_PROPERTY},
+      ${PROPERTY_NAME}: {}
+    };`,
+  },
+]
+
 const invalidCases = [
-  // Without comment at all
+  ...baseInvalidCases,
   {
-    code: `const store = {
-              ${VUEX_STORE_CORE_PROPERTY},
-              ${PROPERTY_NAME}: {
-                  initState() {}
-              }
-          };`,
-    errors: [
-      {
-        message: ERROR_MESSAGE,
-      },
-    ],
-  },
-  {
-    code: `const store = {
-              ${VUEX_STORE_CORE_PROPERTY},
-              ${PROPERTY_NAME}: {
-                  initState: () => {}
-              }
-          };`,
-    errors: [
-      {
-        message: ERROR_MESSAGE,
-      },
-    ],
-  },
-  {
-    code: `const store = {
-              ${VUEX_STORE_CORE_PROPERTY},
-              ${PROPERTY_NAME}: {
-                  initState: function initState() {}
-              }
-          };`,
-    errors: [
-      {
-        message: ERROR_MESSAGE,
-      },
-    ],
-  },
-  // Empty comment Line
-  {
+    title: "jsdoc is empty comment line",
     code: `const store = {
               ${VUEX_STORE_CORE_PROPERTY},
               ${PROPERTY_NAME}: {
@@ -142,48 +144,14 @@ const invalidCases = [
       },
     ],
   },
-  // Empty comment Block
   {
+    title: "jsdoc is empty comment block",
     code: `const store = {
               ${VUEX_STORE_CORE_PROPERTY},
               ${PROPERTY_NAME}: {
                   /*        */
                   initState() {},
               }
-          };`,
-    errors: [
-      {
-        message: ERROR_MESSAGE,
-      },
-    ],
-  },
-  // Store creator function w/o comment
-  {
-    code: `function createStore() {
-              return {
-                  ${VUEX_STORE_CORE_PROPERTY},
-                  ${PROPERTY_NAME}: {
-                      initState() {}
-                  }
-              };
-          }`,
-    errors: [
-      {
-        message: ERROR_MESSAGE,
-      },
-    ],
-  },
-  // es6 property shorthand syntax
-  {
-    code: `
-          const justVariableForSettingUpSomeScope = '';
-          const ${PROPERTY_NAME} = {
-                  initState: function initState ()  {}
-          }
-          
-          const store = {
-              ${VUEX_STORE_CORE_PROPERTY},
-              ${PROPERTY_NAME}
           };`,
     errors: [
       {
